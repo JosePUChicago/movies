e3951a5501c37525639d85795951e22a854a1031
Raghu Betina <raghu@firstdraft.com>
2017-03-16 19:02:35 +0000

    Direct association between users and movie_watchlists

--- app/models/movie_watchlist.rb
+++ app/models/movie_watchlist.rb
@@ -1,6 +1,9 @@
 class MovieWatchlist < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :counter_cache => :interests_count
+
   belongs_to :movie,
              :counter_cache => :interests_count
 
