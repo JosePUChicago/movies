3e861c26a113bbd9f76cbca7cd605ddca95a0ff1
Raghu Betina <raghu@firstdraft.com>
2017-03-16 19:02:58 +0000

    Whitelist columns in Theater for ActiveAdmin

--- app/admin/theater.rb
+++ app/admin/theater.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Theater do
 
+ permit_params :address, :name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
