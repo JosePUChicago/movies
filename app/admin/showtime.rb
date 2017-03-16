299d0b3ea30cdc57062be824b3b6ad5dd666415e
Raghu Betina <raghu@firstdraft.com>
2017-03-16 19:03:02 +0000

    Whitelist columns in Showtime for ActiveAdmin

--- app/admin/showtime.rb
+++ app/admin/showtime.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Showtime do
 
+ permit_params :datetime, :time, :movie_id, :theater_id, :quals, :ticketurl
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
